3a5899783f981d27234e9c318002b7ef96f238d7
badieh1 <badieh.hamid@stud.hs-hannover.de>
2026-09-23 13:00:09 +0200

    Add fourth Service Request for incremental test

--- models/source_service_requests.sql
+++ models/source_service_requests.sql
@@ -28,3 +28,13 @@ SELECT
     TIMESTAMP '2026-09-03 09:15:00',
     TIMESTAMP '2026-09-23 10:00:00',
     'NYC_TEST'
+    
+UNION ALL
+
+SELECT
+    '1004',
+    'Water Leak',
+    'Open',
+    TIMESTAMP '2026-09-04 08:45:00',
+    TIMESTAMP '2026-09-23 11:00:00',
+    'NYC_TEST'
