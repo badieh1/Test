bb26202e592f9ffedacf7b9e0bd5c9e086b09d27
badieh1 <badieh.hamid@stud.hs-hannover.de>
2026-09-23 13:17:00 +0200

    Simulate Service Request status change

--- models/source_service_requests.sql
+++ models/source_service_requests.sql
@@ -4,9 +4,9 @@
 SELECT
     '1001' AS unique_key,
     'Noise - Residential' AS complaint_type,
-    'Open' AS status,
+    'Closed' AS status,
     TIMESTAMP '2026-09-01 10:00:00' AS created_date,
-    TIMESTAMP '2026-09-23 10:00:00' AS ldts,
+    TIMESTAMP '2026-09-23 12:00:00' AS ldts,
     'NYC_TEST' AS rsrc
 
 UNION ALL
